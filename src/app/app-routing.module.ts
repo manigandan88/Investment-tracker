146eb0212d21ab34f729818f3adf7c446796beeb
manigandan88 <manigan345@gmail.com>
2025-08-01 16:38:52 +0530

    test

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+
+const routes: Routes = [
+
+    {path: '', component: Page1Component}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
